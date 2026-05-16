160965c85e82e3c583034b2a534e53e4c745fb63
Simon Yan <xiusmo@gmail.com>
2026-05-16 09:44:41 +0800

    Bump app version to 0.1.1

--- scripts/build-app.sh
+++ scripts/build-app.sh
@@ -11,7 +11,7 @@ cd "$ROOT"
 CONFIG="${CONFIG:-release}"
 APP_NAME="Codex Accounts"
 APP_BUNDLE_ID="${APP_BUNDLE_ID:-dev.codexaccounts.app}"
-APP_VERSION="${APP_VERSION:-0.1.0}"
+APP_VERSION="${APP_VERSION:-0.1.1}"
 
 echo "==> swift build -c $CONFIG"
 swift build -c "$CONFIG"
